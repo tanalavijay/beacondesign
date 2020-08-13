54611826b779be82c977a19a1329ce1d9bd4bbf6
vijay <vijaykumartanala@gmail.com>
2020-08-13 17:19:14 +0530

    assets master

---
+++ src/app/beacon/assets/asset-master/addasset-gridcolumns/addasset-gridcolumns.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { FormControl, FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-addasset-gridcolumns',
+  templateUrl: './addasset-gridcolumns.component.html',
+  styleUrls: ['./addasset-gridcolumns.component.scss']
+})
+export class AddassetGridcolumnsComponent implements OnInit {
+
+  gridColumnForm: FormGroup;
+  
+  constructor(public fb:FormBuilder,@Inject(MAT_DIALOG_DATA) public project: any ,public dialogRef: MatDialogRef<AddassetGridcolumnsComponent>) { 
+    this.createGridColumnGroup();
+  }
+
+
+  ngOnInit() {
+    this.gridColumnForm.controls['campaignName'].setValue(parseInt('1'));
+    this.gridColumnForm.controls['Industry'].setValue(parseInt('1'));
+    this.gridColumnForm.controls['Segment'].setValue(parseInt('1'));
+    this.gridColumnForm.controls['Country'].setValue(parseInt('1'));
+    this.gridColumnForm.controls['CampaignType'].setValue(parseInt('1'));
+    this.gridColumnForm.controls['Solution'].setValue(parseInt('1'));
+    this.gridColumnForm.controls['Checklist'].setValue(parseInt('1'));
+    this.gridColumnForm.controls['LeadSample'].setValue(parseInt('1'));
+    this.gridColumnForm.controls['EstimatedLeads'].setValue(parseInt('1'));
+    this.gridColumnForm.controls['ActualLeads'].setValue(parseInt('1'));
+  }
+  
+  createGridColumnGroup() {
+    this.gridColumnForm = this.fb.group({
+      campaignName : new FormControl(null),
+      Industry : new FormControl(null),
+      Segment : new FormControl(null),
+      Country : new FormControl(null),
+      CampaignType : new FormControl(null),
+      Solution : new FormControl(null),
+      Checklist : new FormControl(null),
+      LeadSample : new FormControl(null),
+      EstimatedLeads : new FormControl(null),
+      ActualLeads : new FormControl(null),
+
+    })
+  }
+  close(): void {
+    this.dialogRef.close();
+  }
+
+  saveGridColumns() {
+    this.dialogRef.close();
+  }
+
+}
--- src/app/beacon/assets/asset-master/asset-master.component.ts
+++ src/app/beacon/assets/asset-master/asset-master.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DeleteConfirmDialogComponent } from 'src/app/shared/delete-confirm-dialog/delete-confirm-dialog.component';
+import { MatDialog } from '@angular/material';
+import { AddassetGridcolumnsComponent } from './addasset-gridcolumns/addasset-gridcolumns.component';
+import { AddassetDialogComponent } from './addasset-dialog/addasset-dialog.component';
 
 @Component({
   selector: 'app-asset-master',
@@ -6,10 +10,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./asset-master.component.scss']
 })
 export class AssetMasterComponent implements OnInit {
+  public popoverStatusTitle: string = 'Confirm Status Change';
+  public popoverStatusMessage: string = 'Are you sure you want to change status?';
+  public cancelClicked: boolean = false;
+  public showEmpty: boolean = false;
+  filterToggle = false;
 
-  constructor() { }
-
+  constructor(public dialog : MatDialog) { }
+  assets=[{cat:"Computers",seqnumber:"PC-0001",assnumber:"Asset-0001",assname:"Board",make:"Sample",model:"Sample",price:"$250.00",date:"12/08/2020",desc:"Testing",dealer:"Automobiles & Services",Status:true},
+  {cat:"Automobiles",seqnumber:"AM-0002",assnumber:"Asset-0001",assname:"Board",make:"Sample",model:"Sample",price:"$250.00",date:"12/08/2020",desc:"Testing",dealer:"Automobiles & Services",Status:true},
+  {cat:"Data Collectors",seqnumber:"DC-0001",assnumber:"Asset-0001",assname:"Board",make:"Sample",model:"Sample",price:"$250.00",date:"12/08/2020",desc:"Testing",dealer:"Automobiles & Services",Status:false},
+  {cat:"GPS",seqnumber:"GPS-0001",assnumber:"Asset-0001",assname:"Board",make:"Sample",model:"Sample",price:"$250.00",date:"12/08/2020",desc:"Testing",dealer:"Automobiles & Services",Status:true},
+  {cat:"Computers",seqnumber:"PC-0001",assnumber:"Asset-0001",assname:"Board",make:"Sample",model:"Sample",price:"$250.00",date:"12/08/2020",desc:"Testing",dealer:"Automobiles & Services",Status:true}]
+  
   ngOnInit() {
+
   }
 
+  public openCampaignDialog(Campaign) {
+    const dialogRef = this.dialog.open(AddassetDialogComponent, {
+      data: Campaign,
+      height: 'auto',
+      width: '600px',
+      autoFocus: false
+    });
+
+    dialogRef.afterClosed().subscribe(campaign => {
+      if (campaign === 'save') {
+        
+      } else {
+      }
+    });
+  }
+
+  openGridColumnsDialog(stat) {
+    let dialogRef = this.dialog.open(AddassetGridcolumnsComponent, {
+      data: stat,
+      height: 'auto',
+      width: '600px',
+      autoFocus: false,
+    });
+
+    dialogRef.afterClosed().subscribe(prospects => {
+      if (prospects == 'save') {
+        
+      }
+    });
+  }
+
+  dowloadBulkUploadCampaignTemplate(item) {}
+
+   openDeleteDialog(value) {
+    let dialogRef = this.dialog.open(DeleteConfirmDialogComponent, {
+      data: value,
+      height: 'auto',
+      width: 'auto',
+      autoFocus: false
+    });
+    dialogRef.afterClosed().subscribe(data => {
+      if (value != null && value !== undefined) {
+      
+      }
+    });
+   }
 }
